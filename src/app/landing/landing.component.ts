d7d28d18e8e8b4985da32bca1eb1910a0bfe5b5a
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2023-02-02 18:34:54 +0900

    main
    main

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,6 +3,11 @@ import { OktaWidgetService } from '../shared/okta-widget.service';
 import { OktaConfigService } from '../shared/okta-config.service';
 import { ViewEncapsulation } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { OktaGetTokenService } from '../shared/okta-get-token.service';
+import { OktaSDKAuthService } from '../shared/okta-auth.service';
+import { OktaAuth } from '@okta/okta-auth-js'
+
+
 
 
 
@@ -13,14 +18,42 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   encapsulation: ViewEncapsulation.None
 })
 export class LandingComponent {
-  
+  strUserSession;
+  strThisUser;
+  public authService = new OktaAuth(this.OktaSDKAuthService.config);
+
   constructor(
     private OktaWidgetService: OktaWidgetService,
     private OktaConfigService: OktaConfigService,
-  ) {  }
-    async ngOnInit() {
+    private OktaGetTokenService: OktaGetTokenService,
+    private OktaSDKAuthService: OktaSDKAuthService,
+
+  ) { }
+  async ngOnInit() {
     await this.OktaWidgetService.CloseWidget();
     await this.OktaWidgetService.login(this.OktaConfigService.strRedirectURL);
+
+    this.strUserSession = await this.authService.isAuthenticated();
+    switch (this.strUserSession == true) {
+      case false: {
+        await this.OktaWidgetService.CloseWidget();
+        await this.OktaWidgetService.login(this.OktaConfigService.strRedirectURL)
+        break;
+      }
+      case true: {
+        this.strThisUser = await this.authService.token.getUserInfo()
+          .then(function (user) {
+            return user
+          })
+          .catch((err) => {
+            console.log(err);
+            window.location.replace(this.OktaConfigService.strPostLogoutURL);
+          })
+
+
+        break;
+      }
+    }
   }
 
 }
